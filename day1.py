3084469891a4cf2b4d0bfdd453a3f82b135e9777
Gautam Roy <zademathews@gmail.com>
2023-11-09 01:18:23 +0000

    Input function + len function

--- day1.py
+++ day1.py
@@ -1,1 +1,9 @@
-print("Gautam" + "Roy") #no space / String concatenation
+print("What is your name? ")
+input("Type your name: ")
+
+
+print("Hello " + input("What is your name? "))
+
+
+# len function
+print(len(input("What is your name? ")))
