4bfe0d47a53bfff6167bbbd7163f189055d4b823
Gautam Roy <zademathews@gmail.com>
2023-11-13 09:59:49 +0000

    Complete project without images

--- day4f.py
+++ day4f.py
@@ -1,5 +1,22 @@
 # Project - Rock, Paper, Scissors
 
+import random
 
-user_selection = input("Choose a number. 0 for rock, 1 for paper, 2 for scrissors")
+selections = ["rock", "paper", "scissors"]
+user_selection = int(input("Choose a number. 0 for rock, 1 for paper, 2 for scissors: "))
 
+computer_selection = random.randint(0,2)
+
+if user_selection > 2 or user_selection < 0:
+    print("Invalid choice! Exiting game!")
+    exit(0)
+
+print(f"You chose: {selections[user_selection]}")
+print(f"Computer chose: {selections[computer_selection]}")
+
+if user_selection == computer_selection:
+    print("It's a tie!")
+elif (user_selection == 0 and computer_selection == 2) or (user_selection == 1 and computer_selection == 0) or (user_selection == 2 and computer_selection == 1):
+    print("You win!")
+else:
+    print("You lose!")
